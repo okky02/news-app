67f141a1538c49636a2010ba2d9afc8c97abccff
okky02 <okkydhelfilano@gmail.com>
2025-11-11 13:45:20 +0700

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import ScrollTopButton from "./components/ScrollTopButton";
 import Home from "./pages/Home";
+import Detail from "./pages/Detail";
 
 function App() {
   return (
-    <div>
-      <Home/>
-    </div>
+    <>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail" element={<Detail />} />
+        </Routes>
+      </BrowserRouter>
+      <ScrollTopButton />
+    </>
   );
 }
 
